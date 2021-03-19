43879ea71f1e00795cb0b3a889f19492377a4f3f
Juan Torres <juan.m13@hotmail.fr>
2021-03-19 11:28:13 +0100

    test

--- page_produit1.php
+++ page_produit1.php
@@ -114,23 +114,6 @@
     </div>
 </div>
 
-<!---js 4 toogle menu-->
-<script>
-    var MenuItems = document.getElementById("MenuItems");
-
-    MenuItems.style.maxHeight = "0px";
-
-    function menutoggle(){
-        if(MenuItems.style.maxHeight == "0px")
-        {
-            MenuItems.style.maxHeight = "200px";
-        }
-        else
-        {
-            MenuItems.style.maxHeight = "0px";
-        }
-    }
-</script>
 
 
 <hr>
