61b2d1b5c6c91ab849da229ed644a4f2aef3a99e
Hairak <77623242+Hairak@users.noreply.github.com>
2021-04-11 00:59:34 +0200

    ajout du lien produit dans le header

--- config/template/header.php
+++ config/template/header.php
@@ -2,6 +2,7 @@
 <a class="nav-link" href="index.php"><h1>ORĐER</h1></a>
 
 <nav class="nav">
+    <a class="nav-link" href="fiche_produit.php">Produits</a>
     <a class="nav-link" href="inscription.php">Inscription</a>
     <a class="nav-link" href="login.php">Login</a>
     <a class="nav-link" href="profil.php"><i class="fas fa-user fa-lg"></i></a>
