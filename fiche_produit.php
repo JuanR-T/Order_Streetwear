6bba7f8d393bd4fa3cc57c349bb517eb3496ee6e
Hairak <77623242+Hairak@users.noreply.github.com>
2021-04-11 00:54:56 +0200

    ajout de l'html

--- fiche_produit.php
+++ fiche_produit.php
@@ -4,4 +4,68 @@
 </header>
 <h2 class="text-center mt-5 mb-5">Page fiche produit</h2>
 <hr>
+<!----- Tous les produits----->
+<div class="small-container">
+
+    <div class="row row-2">
+        <h2>Tous nos produits</h2>
+        <select>
+            <option>Sélection par défaut</option>
+            <option>Sélection par le prix</option>
+            <option>Les produits populaires</option>
+            <option>Les produits les mieux notés</option>
+            <option>Les produits les plus vendus</option>
+        </select>
+    </div>
+<!--- <h2 class="title">Tous nos produits</h2>--> 
+<div class="row">
+        <div class="col-4">
+           <img src="https://bit.ly/2NqyPB8" alt=""> 
+        <h4>Nos sweats</h4>
+        <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+        </div>
+        <p>49.99€</p>
+        </div>
+        <div class="col-4">
+            <img src="https://bit.ly/2NqyPB8" alt=""> 
+         <h4>Nos sweats</h4>
+         <div class="rating">
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star-o"></i>
+         </div>
+         <p>49.99€</p>
+         </div>
+         <div class="col-4">
+            <img src="https://bit.ly/2NqyPB8" alt=""> 
+         <h4>Nos sweats</h4>
+         <div class="rating">
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star-o"></i>
+         </div>
+         <p>49.99€</p>
+         </div>
+         <div class="col-4">
+            <img src="https://bit.ly/2NqyPB8" alt=""> 
+         <h4>Nos sweats</h4>
+         <div class="rating">
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star"></i>
+             <i class="fa fa-star-o"></i>
+         </div>
+         <p>49.99€</p>
+         </div>
+    </div>
 <?php include 'config/template/footer.php'; ?>
